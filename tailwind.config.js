27d6daa363157558628a02c60ad4046ae6baab95
yewyewXD <yewyew6933@hotmail.com>
2025-08-07 21:37:20 +0800

    add primary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#60a5fa',
+          normal: '#2563eb',
+          dark: '#1f2937',
+        },
+      },
+    },
   },
   plugins: [],
 }
