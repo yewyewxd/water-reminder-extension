0e5a35a929b84f6c40254e3692c7606c85a2bbb5
yewyewXD <yewyew6933@gmail.com>
2025-08-09 10:31:54 +0800

    ui for goal setting view

--- src/popup.js
+++ src/popup.js
@@ -1,15 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Switch page logic
   const notificationBtn = document.getElementById('notification-button')
-  const backBtn = document.getElementById('back-button')
+  const notificationBackBtn = document.getElementById(
+    'notification-view-back-button'
+  )
+  const goalBtn = document.getElementById('goal-setting-button')
+  const goalBackBtn = document.getElementById('goal-view-back-button')
   // -- to Notification View
   notificationBtn.addEventListener('click', () => {
     document.getElementById('main-view').classList.add('hidden')
     document.getElementById('notification-view').classList.remove('hidden')
   })
+  // -- to Goal Setting View
+  goalBtn.addEventListener('click', () => {
+    document.getElementById('main-view').classList.add('hidden')
+    document.getElementById('goal-view').classList.remove('hidden')
+  })
   // -- to Main View
-  backBtn.addEventListener('click', () => {
+  notificationBackBtn.addEventListener('click', () => {
     document.getElementById('notification-view').classList.add('hidden')
+    document.getElementById('main-view').classList.remove('hidden')
+  })
+  goalBackBtn.addEventListener('click', () => {
+    document.getElementById('goal-view').classList.add('hidden')
     document.getElementById('main-view').classList.remove('hidden')
   })
 
@@ -103,8 +116,10 @@ document.addEventListener('DOMContentLoaded', () => {
       notificationActiveText.classList.add('hidden')
     }
     notificationSaveBtn.innerText = 'Saved!'
+    notificationSaveBtn.style.opacity = 0.8
     setTimeout(() => {
       notificationSaveBtn.innerText = 'Save'
-    }, 1000)
+      notificationSaveBtn.style.removeProperty('opacity')
+    }, 2000)
   })
 })
