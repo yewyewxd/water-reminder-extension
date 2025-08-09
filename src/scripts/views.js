6af4adc55fc0ef8a489fd05bc5f79c46718f577f
yewyewXD <yewyew6933@gmail.com>
2025-08-09 11:45:33 +0800

    refactor repeated getElementById

--- src/scripts/views.js
+++ src/scripts/views.js
@@ -1,28 +1,36 @@
 export default function () {
   // Switch page logic
+  const mainView = document.getElementById('main-view')
+  const notificationView = document.getElementById('notification-view')
+  const goalView = document.getElementById('goal-view')
+
+  // -- to Notification View
   const notificationBtn = document.getElementById('notification-button')
+  notificationBtn.onclick = function () {
+    mainView.classList.add('hidden')
+    notificationView.classList.remove('hidden')
+  }
+
+  // -- to Goal Setting View
+  const goalBtn = document.getElementById('goal-setting-button')
+  goalBtn.onclick = function () {
+    mainView.classList.add('hidden')
+    goalView.classList.remove('hidden')
+  }
+
+  // -- from Notification to Main View
   const notificationBackBtn = document.getElementById(
     'notification-view-back-button'
   )
-  const goalBtn = document.getElementById('goal-setting-button')
+  notificationBackBtn.onclick = function () {
+    notificationView.classList.add('hidden')
+    mainView.classList.remove('hidden')
+  }
+
+  // -- from Goal Setting View to Main View
   const goalBackBtn = document.getElementById('goal-view-back-button')
-  // -- to Notification View
-  notificationBtn.addEventListener('click', () => {
-    document.getElementById('main-view').classList.add('hidden')
-    document.getElementById('notification-view').classList.remove('hidden')
-  })
-  // -- to Goal Setting View
-  goalBtn.addEventListener('click', () => {
-    document.getElementById('main-view').classList.add('hidden')
-    document.getElementById('goal-view').classList.remove('hidden')
-  })
-  // -- to Main View
-  notificationBackBtn.addEventListener('click', () => {
-    document.getElementById('notification-view').classList.add('hidden')
-    document.getElementById('main-view').classList.remove('hidden')
-  })
-  goalBackBtn.addEventListener('click', () => {
-    document.getElementById('goal-view').classList.add('hidden')
-    document.getElementById('main-view').classList.remove('hidden')
-  })
+  goalBackBtn.onclick = function () {
+    goalView.classList.add('hidden')
+    mainView.classList.remove('hidden')
+  }
 }
